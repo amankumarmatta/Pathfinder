75ced4753435d08a19cae9ecc1ce70e238f55a61
Matta Aman Kumar <amankumarmatta0@gmail.com>
2023-12-24 01:16:40 +0530

    Fixed The Player

--- Assets/Scripts/TerrainHeightProvider.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class TerrainHeightProvider : MonoBehaviour
-{
-    public float GetTerrainHeight(Vector3 position)
-    {
-        return Terrain.activeTerrain.SampleHeight(position);
-    }
-}
